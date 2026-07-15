d4091cf16a98bb48824791371b2d6d8a57174cbb
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-15 14:19:40 +0200

    chore: bump version to 1.1.1

--- _meta.lua
+++ _meta.lua
@@ -1,7 +1,7 @@
 local _ = require("gettext")
 return {
     name        = "coursdechecs",
-    version     = "1.1.0",
+    version     = "1.1.1",
     fullname    = _("Cours d'échecs"),
     description = _("Puzzles et leçons d'échecs"),
 }
